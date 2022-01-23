a0f8d5df20b228cfedd9a24e45a4366dbeb5c556
stephen eshelman, objectthink <stephen.n.eshelman@objectthink.com>
2022-01-23 16:16:28 -0500

    Initial Commit

---
+++ sensors/sensorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  sensorsApp.swift
+//  sensors
+//
+//  Created by stephen eshelman on 1/23/22.
+//
+
+import SwiftUI
+
+@main
+struct sensorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
